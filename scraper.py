ec15e08de89b4aedabbd9ea21df20e079e7926e9
Andy <Andy PC>
2016-07-19 23:32:39 -0700

    test files

--- scraper.py
+++ scraper.py
@@ -3,14 +3,17 @@ import csv
 import re
 from textblob.classifiers import NaiveBayesClassifier
 
-
 # Helper method for retuning a dataset for the classifier
-def dataset(name):
+
+
+def datasetboa(name):
 	reader = csv.DictReader(open(name))
 	return [(re.compile('|'.join(map(re.escape, row['Address'].split()))).sub('', row['Payee']), row["Type"]) for row in reader]
 
-def main():
-	pass
+
+def datasetchase(name):
+	reader = csv.DictReader(open(name))
+	return [(row['Description'], row['Type']) for row in reader]
 
 
 def boa(filename):
@@ -20,8 +23,8 @@ def boa(filename):
 	:return: return an array of all statements with the addition of a type
 	"""
 	#training data and test data
-	trainingSet = dataset('training.csv')
-	testSet = dataset('test.csv')
+	trainingSet = datasetboa('training.csv')
+	testSet = datasetboa('test.csv')
 
 	#Train the Classifier
 	cl = NaiveBayesClassifier(trainingSet)
@@ -33,13 +36,33 @@ def boa(filename):
 		row['Type'] = cl.classify(row['Payee'])
 		print row
 
-def chase():
-	pass
+
+def chase(filename):
+	"""
+
+	For chase style statement CSV file
+	:param filename:
+	:return: return an array of all statements with the addition of type
+	"""
+	trainingSet = datasetchase('stmt_chase.csv')
+	cl = NaiveBayesClassifier(trainingSet)
+	# print "Accuracy using test.csv %f" %cl.accuracy(testSet)
+	stmt_reader = csv.DictReader(open(filename))
+	for row in stmt_reader:
+		row['Type'] = cl.classify(row['Description'])
+		print row
 
 
 def ae():
 	pass
 
+#set up dictionary for actions
+choice = {
+	"boa": boa,
+	"chase": chase,
+	"ae": ae
+}
 
 if __name__ == '__main__':
-	boa('stmt_boa.csv')
+	chase('stmt_chase.csv')
+	# boa('stmt_boa.csv')
